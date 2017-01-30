f4801704041095c80314251a19d63337181b46e8
thijsbenschop <thijsbenschop@gmail.com>
2017-01-30 14:26:14 +0100

    use directly from inp

--- R/GUIfunctions.R
+++ R/GUIfunctions.R
@@ -637,7 +637,7 @@ writeSafeFile <- function(obj, format, randomizeRecords, fileOut, ...) {
       dat <- addVarLabels(dat, lab=new_labs)
     }
     write_dta(data=dat, path=fileOut)
-    writeDoFile(obj = obj, valLab = new_labs, fileName = "default", fileOut = paste0(substr(fileOut, 1, nchar(fileOut) - 4), ".txt")) 
+    writeDoFile(obj = obj, valLab = inp$lab, fileName = "default", fileOut = paste0(substr(fileOut, 1, nchar(fileOut) - 4), ".txt")) 
   }
   if (format=="csv") {
     inp <- list(...)
