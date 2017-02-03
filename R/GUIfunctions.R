87294afff0be249b56996f9ef322f386a9aa8ab8
thijsbenschop <thijsbenschop@gmail.com>
2017-02-03 09:29:45 +0100

    add lab

--- R/GUIfunctions.R
+++ R/GUIfunctions.R
@@ -69,23 +69,22 @@ setMethod(f="updateValueLabelX", signature=c("sdcMicroObj"), definition=function
     #obj <- set.sdcMicroObj(obj, type="manipKeyVars", input=list(manipKey))
     obj@manipKeyVars <- manipKey
 
-    # # Update lab info by removing the before levels and adding the after level
-    # labinfo <- lab[[2]][[which(names(lab[[2]]) == var)]]
-    # 
-    # if(length(before) == 1){
-    #   names(labinfo)[which(labinfo == before[1])] <- after[1]
-    #   labinfo[which(labinfo == before[1])] <- numCode
-    # }else{
-    #   names(labinfo)[which(labinfo == before[1])] <- after[1]
-    #   labinfo[which(labinfo == before[1])] <- numCode
-    #   labinfo <- labinfo[-which(labinfo == before[-1])]
-    # }
-    # lab[[2]][[which(names(lab[[2]]) == var)]] <- labinfo
-    # # return(list(obj, lab))
-  # }else{
-  #   return(list(obj, lab))
-  # }
-  return(obj)
+    # Update lab info by removing the before levels and adding the after level
+    labinfo <- lab[[2]][[which(names(lab[[2]]) == var)]]
+    
+    if(length(before) == 1){
+     names(labinfo)[which(labinfo == before[1])] <- after[1]
+     labinfo[which(labinfo == before[1])] <- numCode
+    }else{
+     names(labinfo)[which(labinfo == before[1])] <- after[1]
+     labinfo[which(labinfo == before[1])] <- numCode
+     labinfo <- labinfo[-which(labinfo == before[-1])]
+    }
+    lab[[2]][[which(names(lab[[2]]) == var)]] <- labinfo
+    return(obj) # # return(list(obj, lab))
+  }else{
+    return(obj) #   return(list(obj, lab))
+  }
 })
 
 # Writes a do file to add value labels to a Stata file
