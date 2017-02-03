fb8932555ca24b3e636803077836b218b780c8fd
thijsbenschop <thijsbenschop@gmail.com>
2017-02-03 11:26:03 +0100

    seperate observer

--- inst/shiny/sdcApp/server.R
+++ inst/shiny/sdcApp/server.R
@@ -1186,23 +1186,28 @@ shinyServer(function(session, input, output) {
   # event to update/modify an existing factor variable
   observeEvent(input$btn_update_recfac, {
     ptm <- proc.time()
-    #res <- code_groupAndRename_keyvar()
-    res2 <- code_updateLevel_keyvar()
-    res3 <- code_updateLabel_keyvar()
     res <- code_groupAndRename_keyvar()
-    
     runEvalStr(cmd=res$cmd, comment="## Recode variable")
-    if(!is.null(obj$stata_labs)){
-     #res2 <- code_updateLevel_keyvar()
-     runEvalStr(cmd=res2$cmd, comment="## Update levels after recoding")
-     #res3 <- code_updateLabel_keyvar()
-     runEvalStrLab(cmd=res3$cmd, comment="## Update value labels after recoding")
-    } 
+    # if(!is.null(obj$stata_labs)){
+    #  res2 <- code_updateLevel_keyvar()
+    #  runEvalStr(cmd=res2$cmd, comment="## Update levels after recoding")
+    #  res3 <- code_updateLabel_keyvar()
+    #  runEvalStrLab(cmd=res3$cmd, comment="## Update value labels after recoding")
+    # } 
     ptm <- proc.time()-ptm
     obj$comptime <- obj$comptime+ptm[3]
     if (is.null(lastError())) {
       obj$lastaction <- res$txt_action
       obj$anon_performed <- c(obj$anon_performed, res$txt_action)
+    }
+  })
+  
+  observeEvent(input$btn_update_recfac, {
+    if(!is.null(obj$stata_labs)){
+      res2 <- code_updateLevel_keyvar()
+      runEvalStr(cmd=res2$cmd, comment="## Update levels after recoding")
+      res3 <- code_updateLabel_keyvar()
+      runEvalStrLab(cmd=res3$cmd, comment="## Update value labels after recoding")
     }
   })
 
